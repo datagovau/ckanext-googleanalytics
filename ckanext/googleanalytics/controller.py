15f772d9b0bae5275bd5ab3915ddfb7bdbad596e
Gregory von Nessi <gvn@Derp-E.local>
2017-01-17 15:28:10 +1100

    - [SXTPDFINXZCB-260] - Fixed some indenting errors

--- ckanext/googleanalytics/controller.py
+++ ckanext/googleanalytics/controller.py
@@ -1,17 +1,15 @@
-from ckan.lib.base import c, request
-
+import hashlib
+import logging
+import re
 import urllib
 
-import logging
 import ckan.logic as logic
-import hashlib
-import plugin
-import re
+from ckan.controllers.api import ApiController
+from ckan.lib.base import c, request
+from paste.util.multidict import MultiDict
 from pylons import config
 
-from paste.util.multidict import MultiDict
-
-from ckan.controllers.api import ApiController
+import plugin
 
 log = logging.getLogger('ckanext.googleanalytics')
 
@@ -31,11 +29,11 @@ class GAApiController(ApiController):
                 "dp": c.environ['PATH_INFO'],
                 "dr": c.environ.get('HTTP_REFERER', ''),
                 "ec": "CKAN API Request",
-                "ea": request_obj_type+request_function,
+                "ea": request_obj_type + request_function,
                 "el": request_id,
             }
             data = urllib.urlencode(data_dict)
-            log.debug("Sending API event to Google Analytics: "+data)
+            log.debug("Sending API event to Google Analytics: " + data)
             # send analytics asynchronously
             plugin.GoogleAnalyticsPlugin.analytics_queue.put(data_dict)
 
@@ -53,63 +51,68 @@ class GAApiController(ApiController):
                     id = request_data['query']
                 if 'resource_id' in request_data:
                     id = request_data['resource_id']
-		if 'sql' in request_data:
-		    uuidregex = re.compile('[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}')
-		    for uuid in uuidregex.findall(request_data['sql']):
-			id = uuid
+                if 'sql' in request_data:
+                    uuidregex = re.compile('[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}')
+                    for uuid in uuidregex.findall(request_data['sql']):
+                        id = uuid
                 self._post_analytics(c.user, logic_function, '', id)
         except Exception, e:
-            log.error(e)
+            log.debug(e)
             pass
 
         return ApiController.action(self, logic_function, ver)
 
-    def list(self, ver=None, register=None,
-             subregister=None, id=None):
-        self._post_analytics(c.user,
-                             register +
-                             ("_"+str(subregister) if subregister else ""),
-                             "list",
-                             id)
-        return ApiController.list(self, ver, register, subregister, id)
 
-    def show(self, ver=None, register=None,
-             subregister=None, id=None, id2=None):
-        self._post_analytics(c.user,
-                             register +
-                             ("_"+str(subregister) if subregister else ""),
-                             "show",
-                             id)
-        return ApiController.show(self, ver, register, subregister, id, id2)
+def list(self, ver=None, register=None,
+         subregister=None, id=None):
+    self._post_analytics(c.user,
+                         register +
+                         ("_" + str(subregister) if subregister else ""),
+                         "list",
+                         id)
+    return ApiController.list(self, ver, register, subregister, id)
 
-    def update(self, ver=None, register=None,
-               subregister=None, id=None, id2=None):
-        self._post_analytics(c.user,
-                             register +
-                             ("_"+str(subregister) if subregister else ""),
-                             "update",
-                             id)
-        return ApiController.update(self, ver, register, subregister, id, id2)
 
-    def delete(self, ver=None, register=None,
-               subregister=None, id=None, id2=None):
-        self._post_analytics(c.user,
-                             register +
-                             ("_"+str(subregister) if subregister else ""),
-                             "delete",
-                             id)
-        return ApiController.delete(self, ver, register, subregister, id, id2)
+def show(self, ver=None, register=None,
+         subregister=None, id=None, id2=None):
+    self._post_analytics(c.user,
+                         register +
+                         ("_" + str(subregister) if subregister else ""),
+                         "show",
+                         id)
+    return ApiController.show(self, ver, register, subregister, id, id2)
 
-    def search(self, ver=None, register=None):
-        id = None
-        try:
-            params = MultiDict(self._get_search_params(request.params))
-            if 'q' in params.keys():
-                id = params['q']
-            if 'query' in params.keys():
-                id = params['query']
-        except ValueError, e:
-            log.debug(str(e))
-            pass
-        self._post_analytics(c.user, register, "search", id)
-        return ApiController.search(self, ver, register)
+
+def update(self, ver=None, register=None,
+           subregister=None, id=None, id2=None):
+    self._post_analytics(c.user,
+                         register +
+                         ("_" + str(subregister) if subregister else ""),
+                         "update",
+                         id)
+    return ApiController.update(self, ver, register, subregister, id, id2)
+
+
+def delete(self, ver=None, register=None,
+           subregister=None, id=None, id2=None):
+    self._post_analytics(c.user,
+                         register +
+                         ("_" + str(subregister) if subregister else ""),
+                         "delete",
+                         id)
+    return ApiController.delete(self, ver, register, subregister, id, id2)
+
+
+def search(self, ver=None, register=None):
+    id = None
+    try:
+        params = MultiDict(self._get_search_params(request.params))
+        if 'q' in params.keys():
+            id = params['q']
+        if 'query' in params.keys():
+            id = params['query']
+    except ValueError, e:
+        log.debug(str(e))
+        pass
+    self._post_analytics(c.user, register, "search", id)
+    return ApiController.search(self, ver, register)
